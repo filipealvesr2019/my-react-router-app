628bb4615168bc7b4986652e3f6003e9c68f41ee
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-10-21 17:37:36 -0300

    cm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+
 const fakeDatabase = {
   admin: { email: 'admin@example.com', password: 'admin' },
-  funcionarios: []
+  funcionario: { email: 'funcionario@example.com', password: 'funcionario' }
 };
 
 const Login = () => {
@@ -10,64 +10,29 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [funcionarios, setFuncionarios] = useState([]);
-  
-  useEffect(() => {
-    const funcionariosData = JSON.parse(localStorage.getItem('funcionarios'));
-    if (funcionariosData) {
-      setFuncionarios(funcionariosData);
-    }
-  }, []);
 
-  // Atualizar o localStorage sempre que o estado de funcionarios for alterado
-  useEffect(() => {
-    localStorage.setItem('funcionarios', JSON.stringify(funcionarios));
-  }, [funcionarios]);
   const handleLogin = () => {
     if (email === fakeDatabase.admin.email && password === fakeDatabase.admin.password) {
       setLoggedIn(true);
       setIsAdmin(true);
+    } else if (email === fakeDatabase.funcionario.email && password === fakeDatabase.funcionario.password) {
+      setLoggedIn(true);
+      setIsAdmin(false);
     } else {
-      const funcionario = fakeDatabase.funcionarios.find(
-        (funcionario) => funcionario.email === email && funcionario.password === password
-      );
-
-      if (funcionario) {
-        setLoggedIn(true);
-        setIsAdmin(false);
-      } else {
-        alert('Credenciais inválidas');
-      }
+      alert('Credenciais inválidas');
     }
-  };
-
-  const handleAddFuncionario = (email, password) => {
-    const newFuncionario = { email, password };
-    setFuncionarios([...funcionarios, newFuncionario]);
-  };
-
-  const handleExcluirFuncionario = (index) => {
-    const updatedFuncionarios = funcionarios.filter((_, i) => i !== index);
-    setFuncionarios(updatedFuncionarios);
   };
 
   if (loggedIn) {
     if (isAdmin) {
-      return (
-        <AdminPage
-          funcionarios={funcionarios}
-          handleAddFuncionario={handleAddFuncionario}
-          handleExcluirFuncionario={handleExcluirFuncionario}
-        />
-      );
+      return <AdminPage />;
     } else {
       return <FuncionarioPage />;
     }
   }
 
   return (
-    <div className="flex">
-    <div className='login'>
+    <div>
       <h1>Página de Login</h1>
       <input
         type="text"
@@ -83,48 +48,16 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
       <br />
-      <button className='button' onClick={handleLogin}>Login</button>
-    </div>
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 };
 
-const AdminPage = ({ funcionarios, handleAddFuncionario, handleExcluirFuncionario }) => {
-  const [novoEmail, setNovoEmail] = useState('');
-  const [novaSenha, setNovaSenha] = useState('');
-
-  const adicionarFuncionario = () => {
-    handleAddFuncionario(novoEmail, novaSenha);
-    setNovoEmail('');
-    setNovaSenha('');
-  };
-
+const AdminPage = () => {
   return (
     <div>
       <h2>Página do Admin</h2>
-      <div>
-        <input
-          type="text"
-          placeholder="Email do novo Funcionário"
-          value={novoEmail}
-          onChange={(e) => setNovoEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Senha do novo Funcionário"
-          value={novaSenha}
-          onChange={(e) => setNovaSenha(e.target.value)}
-        />
-        <button onClick={adicionarFuncionario}>Adicionar Funcionário</button>
-      </div>
-      <ul>
-        {funcionarios.map((funcionario, index) => (
-          <li key={index}>
-            {funcionario.email}{' '}
-            <button onClick={() => handleExcluirFuncionario(index)}>Excluir</button>
-          </li>
-        ))}
-      </ul>
+      {/* Lógica para cadastrar e excluir funcionários */}
     </div>
   );
 };
