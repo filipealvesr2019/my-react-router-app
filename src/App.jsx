ce31609df7a3bba2b692ee1bdb1523ae53cb6402
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-10-24 21:35:01 -0300

    cm

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,12 @@
-import React, { useState } from 'react';
-
-const fakeDatabase = {
-  admin: { email: 'admin@example.com', password: 'admin' },
-  funcionario: { email: 'funcionario@example.com', password: 'funcionario' }
-};
-
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  const handleLogin = () => {
-    if (email === fakeDatabase.admin.email && password === fakeDatabase.admin.password) {
-      setLoggedIn(true);
-      setIsAdmin(true);
-    } else if (email === fakeDatabase.funcionario.email && password === fakeDatabase.funcionario.password) {
-      setLoggedIn(true);
-      setIsAdmin(false);
-    } else {
-      alert('Credenciais inválidas');
-    }
-  };
-
-  if (loggedIn) {
-    if (isAdmin) {
-      return <AdminPage />;
-    } else {
-      return <FuncionarioPage />;
-    }
-  }
-
+// src/App.jsx
+import React from 'react';
+import Login from "./Login"
+const App = () => {
   return (
-    <div>
-      <h1>Página de Login</h1>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-      <input
-        type="password"
-        placeholder="Senha"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <>
+      <Login />
+    </>
   );
 };
 
-const AdminPage = () => {
-  return (
-    <div>
-      <h2>Página do Admin</h2>
-      {/* Lógica para cadastrar e excluir funcionários */}
-    </div>
-  );
-};
-
-const FuncionarioPage = () => {
-  return (
-    <div>
-      <h2>Página do Funcionário</h2>
-      {/* Lógica específica para funcionários */}
-    </div>
-  );
-};
-
-export default Login;
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import AdminPage from './AdminPage';
+import FuncionarioPage from './FuncionarioPage';
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/login', {
+        email: email,
+        password: password
+      });
+
+      // Verifique a resposta do backend e defina o estado apropriado com base nela
+      if (response.data.role === 'admin') {
+        setLoggedIn(true);
+        setIsAdmin(true);
+      } else if (response.data.role === 'funcionario') {
+        setLoggedIn(true);
+        setIsAdmin(false);
+      } else {
+        alert('Credenciais inválidas');
+      }
+    } catch (error) {
+      console.error('Erro na solicitação de login', error);
+    }
+  };
+
+  // Renderização condicional com base no estado de loggedIn e isAdmin
+  if (loggedIn) {
+    if (isAdmin) {
+      return <AdminPage />;
+    } else {
+      return <FuncionarioPage />;
+    }
+  }
+
+  return (
+    <div>
+      <h1>Página de Login</h1>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+// Componentes AdminPage e FuncionarioPage devem ser importados aqui
+
+export default Login;
