18efc04677f3a7f97446b32b3f47fb6be650843b
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-10-26 16:35:29 -0300

    cm

--- src/EmployeePage.jsx
+++ src/EmployeePage.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 
 // Página do funcionário
-const FuncionarioPage = () => {
+const EmployeePage = () => {
   // Sua lógica para a página do funcionário aqui
   return <div>Página do Funcionário</div>;
 };
 
-export default FuncionarioPage;
+export default EmployeePage;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import AdminPage from './AdminPage';
-import FuncionarioPage from './FuncionarioPage';
-import  './Login.css';
+import EmployeePage from './EmployeePage';
+import './Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -21,7 +21,7 @@ const Login = () => {
       if (response.data.role === 'admin') {
         setLoggedIn(true);
         setIsAdmin(true);
-      } else if (response.data.role === 'funcionario') {
+      } else if (response.data.role === 'employee') {
         setLoggedIn(true);
         setIsAdmin(false);
       } else {
@@ -37,39 +37,37 @@ const Login = () => {
     if (isAdmin) {
       return <AdminPage />;
     } else {
-      return <FuncionarioPage />;
+      return <EmployeePage />;
     }
   }
 
   return (
     <div className="body">
-    <div className="container">
-    <div className='loginStyle'>
-      <h1>Login</h1>
-      <label for="email">Email</label>
-      <input
-        type="text"
-        placeholder="Digite o email..."
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-      <label for="password">Password</label>
+      <div className="container">
+        <div className='loginStyle'>
+          <h1>Login</h1>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            placeholder="Digite o email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <label htmlFor="password">Password</label>
 
-      <input
-        type="password"
-        placeholder="Digite a senha..."
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      <button className="loginButton"  onClick={handleLogin}>Login</button>
-    </div>
-    </div>
+          <input
+            type="password"
+            placeholder="Digite a senha..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button className="loginButton" onClick={handleLogin}>Login</button>
+        </div>
+      </div>
     </div>
   );
 };
-
-// Componentes AdminPage e FuncionarioPage devem ser importados aqui
 
 export default Login;
