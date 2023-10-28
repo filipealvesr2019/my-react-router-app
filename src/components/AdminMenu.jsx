9d20b558736c83503069b8c5d2a8b764fffc7d3c
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-10-28 12:53:00 -0300

    cm

--- src/components/AdminMenu.jsx
+++ src/components/AdminMenu.jsx
@@ -2,27 +2,19 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './AdminMenu.css';
+import "./AdminMenu.css";
 
 const AdminMenu = () => {
   const [showForm, setShowForm] = useState(false);
 
   const handleButtonClick = () => {
-    setShowForm(true);
-  };
-
-  const handleCloseForm = () => {
-    setShowForm(false);
+    setShowForm(!showForm);
   };
 
   return (
     <div className="admin-menu">
       <button onClick={handleButtonClick}>Adicionar Usuário</button>
-      {showForm && (
-        <div className="modal">
-          <UserForm closeForm={handleCloseForm} />
-        </div>
-      )}
+      {showForm && <UserForm closeForm={() => setShowForm(false)} />}
     </div>
   );
 };
@@ -59,69 +51,85 @@ const UserForm = ({ closeForm }) => {
           pauseOnFocusLoss: true,
           draggable: true,
           pauseOnHover: true,
-          theme: 'light',
+          theme: "light"
         });
         setTimeout(() => {
-          closeForm(); // Fechar o modal após o sucesso
+          closeForm(); // Fechar o formulário após o sucesso
         }, 4000);
       }
     } catch (error) {
       console.error('Erro ao criar usuário', error);
+      toast.error('Erro ao criar usuário', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        rtl: false,
+        pauseOnFocusLoss: true,
+        draggable: true,
+        pauseOnHover: true,
+        theme: "light"
+      });
     }
   };
 
   return (
-    <div>
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <form onSubmit={handleFormSubmit} className="user-form">
-        <label htmlFor="name">Nome</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="text"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <label htmlFor="password">Senha</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <label htmlFor="role">Função</label>
-        <select
-          name="role"
-          value={role}
-          onChange={(e) => setRole(e.target.value)}
-          required
-        >
-          <option value="">Selecione a função</option>
-          <option value="admin">Administrador</option>
-          <option value="employee">Funcionário</option>
-        </select>
-        <button type="submit">Adicionar</button>
-      </form>
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={closeForm}>&times;</span>
+        <div className="modal-form">
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+          <form onSubmit={handleFormSubmit} className="user-form">
+            <label htmlFor="name">Nome</label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Senha</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label htmlFor="role">Função</label>
+            <select
+              name="role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+              required
+            >
+              <option value="">Selecione a função</option>
+              <option value="admin">Administrador</option>
+              <option value="employee">Funcionário</option>
+            </select>
+            <button type="submit">Adicionar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
