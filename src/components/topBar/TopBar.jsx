f3824b88eb2a9dbec921f3696e3a87d905806c0e
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-13 19:05:42 -0300

    cm

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import styles from "./TopBar.module.css"
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import SettingsIcon from '@mui/icons-material/Settings';
 export default function topbar() {
   return (
     <div className={styles.topBar}>
         <div className={styles.topBarWrapper}>
-            <div className={styles.topLeft}>left</div>
-            <div className={styles.topRight}>right</div>
+            <div className={styles.topLeft}><span className={styles.logo}>Painel Administrativo</span></div>
+            <div className={styles.topRight}>
+              <div className={styles.topbarIconContainer}>
+                <NotificationsNoneOutlinedIcon></NotificationsNoneOutlinedIcon>
+                <span className={styles.topIconBag}>
+                  2
+                </span>
+              </div>
+              <div className={styles.topbarIconContainer}>
+                <SettingsIcon></SettingsIcon>
+                <span className={styles.topIconBag}>
+                  2
+                </span>
+              </div>
+            </div>
         </div>
     </div>
   )
