4fb9dfa0395835444defdf407f93b7545a808033
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-11-21 17:51:00 -0300

    ajustes

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,6 +3,8 @@ import UserList from "../../components/UserList";
 import "./Admin.css";
 import AdminMenu from "../../components/AdminMenu";
 export default function Admin() {
+
+  
   return (
     <div className="AdminContainer">
       <AdminMenu className='AdminMenu'></AdminMenu>
@@ -10,7 +12,10 @@ export default function Admin() {
       <div className="h1">
         <h1>Cadastro de Usuarios</h1>
       </div>
+      
       <div className="list">
+        
+        
         <UserList></UserList>
       </div>
     </div>
