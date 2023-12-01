a3024749d29479d39fd51ec8ab12c81dcd77ad4b
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-01 12:12:23 -0300

    AJUSTES

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import "./index.css";
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
+  <AuthProvider>
 
        <App />
+       </AuthProvider>,
 
  </React.StrictMode>
 );
