155819a148cd79b40b81c6f6f0185d333e8d4192
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-20 18:19:40 -0300

    ajustes

--- src/Page2Content.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page2Content = () => {
-  return (
-    <div>Page2Content</div>
-  )
-}
-
-export default Page2Content
--- src/Page3Content.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page3Content = () => {
-  return (
-    <div>Page3Content</div>
-  )
-}
-
-export default Page3Content
