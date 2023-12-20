b19f8f2135cddf2cc8216f82b974c9d918ff9d99
filipealvesr2019 <filipealvesrds2019@gmail.com>
2023-12-20 18:09:45 -0300

    ajustes

--- src/AdminPage.jsx
+++ src/AdminPage.jsx
@@ -1,27 +1,49 @@
-import"./AdminPage.css";
-import { useAuth } from "./context/AuthContext";
+import React, { useRef, useEffect } from 'react';
+import './AdminPage.css';
+import { useAuth } from './context/AuthContext';
 
 const AdminPage = () => {
   const { logout } = useAuth();
-  // Sua lógica para a página do administrador aqui
+  const indicatorRef = useRef(null);
+
+  useEffect(() => {
+    const items = document.querySelectorAll('nav a');
+
+    const marker = (e) => {
+      indicatorRef.current.style.left = e.offsetLeft + 'px';
+      indicatorRef.current.style.width = e.offsetWidth + 'px';
+    };
+
+    items.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        marker(e.target);
+      });
+    });
+
+    return () => {
+      // Remove event listeners when the component unmounts
+      items.forEach((link) => {
+        link.removeEventListener('click', marker);
+      });
+    };
+  }, []); // Empty dependency array ensures this effect runs once after initial render
+
   return (
     <div>
-      <header className="headerMenu">
-        <a href="#" className="logo">Logo</a>
+      <header>
+        <a href="#" className="logo">
+          Logo
+        </a>
         <nav>
-          <div id="indicator"></div>
-        <a href="#" >Home</a>
-        <a href="#">Cadastros</a>
-        <a href="#">Financeiro</a>
-        <a href="#">Estoque</a>
-        <a href="#">Fiscal</a>
-        <a href="#">Relatórios</a>
-
+          <div ref={indicatorRef} id="indicator"></div>
+          <a href="#">Home</a>
+          <a href="#">Cadastros</a>
+          <a href="#">Financeiro</a>
+          <a href="#">Estoque</a>
+          <a href="#">Fiscal</a>
+          <a href="#">Relatórios</a>
         </nav>
       </header>
-   
-
-      <button className="button" onClick={logout}>Logout2</button>
     </div>
   );
 };
