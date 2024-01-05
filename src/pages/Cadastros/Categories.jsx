197a2e107c2b259ff4f9e615e880e11a65241850
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-05 11:59:51 -0300

    add

--- src/pages/Cadastros/Categories.jsx
+++ src/pages/Cadastros/Categories.jsx
@@ -1,6 +1,6 @@
-// Categories.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Categories.css';
 
 const Categories = () => {
     const [categories, setCategories] = useState([]);
@@ -9,6 +9,7 @@ const Categories = () => {
     const [newSubcategory, setNewSubcategory] = useState('');
     const [selectedCategoryId, setSelectedCategoryId] = useState(null);
     const [availableSubcategories, setAvailableSubcategories] = useState([]);
+    const [addedSubcategories, setAddedSubcategories] = useState([]);
 
     useEffect(() => {
         // Ao montar o componente, carrega categorias e subcategorias
@@ -31,21 +32,20 @@ const Categories = () => {
             setSubcategories(response.data.subcategories);
             setSelectedCategoryId(categoryId);
             // Carrega subcategorias disponíveis para adicionar à categoria
-            loadAvailableSubcategories(categoryId);
+            loadAvailableSubcategories();
         } catch (error) {
             console.error('Erro ao obter subcategorias', error);
         }
     };
 
-    const loadAvailableSubcategories = async (categoryId) => {
-      try {
-          const response = await axios.get(`http://localhost:3001/api/admin/subcategories`);
-          setAvailableSubcategories(response.data.subcategories);
-      } catch (error) {
-          console.error('Erro ao obter subcategorias disponíveis', error);
-      }
-  };
-  
+    const loadAvailableSubcategories = async () => {
+        try {
+            const response = await axios.get(`http://localhost:3001/api/admin/subcategories`);
+            setAvailableSubcategories(response.data.subcategories);
+        } catch (error) {
+            console.error('Erro ao obter subcategorias disponíveis', error);
+        }
+    };
 
     const addSubcategoryToCategory = async () => {
         try {
@@ -57,11 +57,97 @@ const Categories = () => {
             if (response.data.success) {
                 setNewSubcategory('');
                 getSubcategories(selectedCategoryId);
+                getAddedSubcategories(selectedCategoryId);
             } else {
                 console.error('Erro ao criar subcategoria. Mensagem do servidor:', response.data.error);
             }
         } catch (error) {
             console.error('Erro ao criar subcategoria', error);
+        }
+    };
+
+    const getAddedSubcategories = async (categoryId) => {
+        try {
+            const response = await axios.get(`http://localhost:3001/api/admin/subcategories?category=${categoryId}`);
+            setAddedSubcategories(response.data.subcategories);
+        } catch (error) {
+            console.error('Erro ao obter subcategorias adicionadas', error);
+        }
+    };
+
+    const addCategory = async () => {
+        try {
+            const response = await axios.post('http://localhost:3001/api/admin/category/new', {
+                name: newCategory,
+            });
+
+            if (response.data.success) {
+                setNewCategory('');
+                getCategories();
+            } else {
+                console.error('Erro ao criar categoria. Mensagem do servidor:', response.data.error);
+            }
+        } catch (error) {
+            console.error('Erro ao criar categoria', error);
+        }
+    };
+
+    const addSubcategory = async () => {
+        try {
+            const response = await axios.post('http://localhost:3001/api/admin/subcategories/new', {
+                name: newSubcategory,
+            });
+
+            if (response.data.success) {
+                setNewSubcategory('');
+                getSubcategories(selectedCategoryId);
+                getAddedSubcategories(selectedCategoryId);
+            } else {
+                console.error('Erro ao criar subcategoria. Mensagem do servidor:', response.data.error);
+            }
+        } catch (error) {
+            console.error('Erro ao criar subcategoria', error);
+        }
+    };
+
+    const handleEditCategory = (category) => {
+        // Implemente a lógica para editar a categoria aqui
+        console.log('Editar categoria:', category);
+    };
+
+    const handleDeleteCategory = async (category) => {
+        try {
+            const response = await axios.delete(`http://localhost:3001/api/admin/categories/${category._id}`);
+
+            if (response.data.success) {
+                console.log('Categoria excluída com sucesso');
+                getCategories();
+            } else {
+                console.error('Erro ao excluir categoria. Mensagem do servidor:', response.data.error);
+            }
+        } catch (error) {
+            console.error('Erro ao excluir categoria', error);
+        }
+    };
+
+    const handleEditSubcategory = (sub) => {
+        // Implemente a lógica para editar a subcategoria aqui
+        console.log('Editar subcategoria:', sub);
+    };
+
+    const handleDeleteSubcategory = async (sub) => {
+        try {
+            const response = await axios.delete(`http://localhost:3001/api/admin/subcategories/${sub._id}`);
+
+            if (response.data.success) {
+                console.log('Subcategoria excluída com sucesso');
+                getSubcategories(selectedCategoryId);
+                getAddedSubcategories(selectedCategoryId);
+            } else {
+                console.error('Erro ao excluir subcategoria. Mensagem do servidor:', response.data.error);
+            }
+        } catch (error) {
+            console.error('Erro ao excluir subcategoria', error);
         }
     };
 
@@ -78,13 +164,9 @@ const Categories = () => {
                 </select>
             </label>
 
-         
-
             {/* Adicionar Subcategoria à Categoria Selecionada */}
             {selectedCategoryId && (
                 <div>
-                  
-                    
                     {/* Dropdown para selecionar subcategoria a adicionar */}
                     <label>
                         Selecione uma Subcategoria:
@@ -95,17 +177,46 @@ const Categories = () => {
                             ))}
                         </select>
                     </label>
-
                     <button onClick={addSubcategoryToCategory}>Adicionar Subcategoria</button>
                 </div>
             )}
 
-            {/* Tabela para Todas Categorias e Subcategorias */}
-            <table>
+            {/* Tabela para Subcategorias Adicionadas à Categoria Selecionada */}
+            <table className="category-table">
+                <thead>
+                    <tr>
+                        <th>Categoria</th>
+                        <th>Subcategoria Adicionada</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {addedSubcategories.map((sub) => (
+                        <tr key={sub._id}>
+                            <td>{sub.category}</td>
+                            <td>{sub.name}</td>
+                            <td>
+                                <button onClick={() => handleEditSubcategory(sub)}>Editar</button>
+                                <button onClick={() => handleDeleteSubcategory(sub)}>Excluir</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <div>
+                <label>
+                    Adicionar Nova Categoria:
+                    <input type="text" value={newCategory} onChange={(e) => setNewCategory(e.target.value)} />
+                    <button onClick={addCategory}>Adicionar Categoria</button>
+                </label>
+            </div>
+
+            {/* Tabela para Todas Categorias */}
+            <table className="category-table">
                 <thead>
                     <tr>
                         <th>Todas Categorias</th>
-                        <th>Todas Subcategorias</th>
                         <th>Ações</th>
                     </tr>
                 </thead>
@@ -114,11 +225,37 @@ const Categories = () => {
                         <tr key={category._id}>
                             <td>{category.name}</td>
                             <td>
-                                {subcategories.map((sub) => (
-                                    <div key={sub._id}>{sub.name}</div>
-                                ))}
+                                <button onClick={() => handleEditCategory(category)}>Editar</button>
+                                <button onClick={() => handleDeleteCategory(category)}>Excluir</button>
                             </td>
-                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <div>
+                <label>
+                    Adicionar Nova Subcategoria:
+                    <input type="text" value={newSubcategory} onChange={(e) => setNewSubcategory(e.target.value)} />
+                    <button onClick={addSubcategory}>Adicionar Subcategoria</button>
+                </label>
+            </div>
+            {/* Tabela para Todas Subcategorias */}
+            <table className="category-table">
+                <thead>
+                    <tr>
+                        <th>Todas Subcategorias</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {subcategories.map((sub) => (
+                        <tr key={sub._id}>
+                            <td>{sub.name}</td>
+                            <td>
+                                <button onClick={() => handleEditSubcategory(sub)}>Editar</button>
+                                <button onClick={() => handleDeleteSubcategory(sub)}>Excluir</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
