bd3e3c6a742d9b05782ebf3bd3ae3e043c08e921
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-01-20 16:35:37 -0300

    add

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -28,7 +28,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await axios.post('https://serveradmin-whhj.onrender.com/login', {
+      const response = await axios.post('https://serveradmin-whhj.onrender.com/api/login', {
         email: email,
         password: password
       });
