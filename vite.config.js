146b59d04bb9361c1c51530bc8459f351f32294c
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-02-01 15:12:59 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'http://localhost:7000/', // Substitua 'seu-novo-host' pelo host desejado
+  },
   
 })
