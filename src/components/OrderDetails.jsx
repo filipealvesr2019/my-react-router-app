614fcf71e9ba12161c27a51cb34fbfd8eee3c124
filipealvesr2019 <filipealvesrds2019@gmail.com>
2024-04-17 19:29:35 -0300

    feat: adicioana compos de quantidade total.

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const OrderDetails = () => {
   const [boleto, setBoleto] = useState(null);
   const [pix, setPix] = useState(null);
   const [creditCard, setCreditCard] = useState(null);
-  
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -31,26 +31,403 @@ const OrderDetails = () => {
       .catch((error) => {
         console.error("Erro ao obter os detalhes do pix:", error);
       });
-        // Requisição para detalhes do pix
+    // Requisição para detalhes do pix
     axios
-    .get(`http://localhost:3001/api/creditCard/${id}`)
-    .then((response) => {
-      setCreditCard(response.data);
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error("Erro ao obter os detalhes do pix:", error);
-    });
+      .get(`http://localhost:3001/api/creditCard/${id}`)
+      .then((response) => {
+        setCreditCard(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao obter os detalhes do pix:", error);
+      });
   }, [id]);
 
   return (
     <div>
-      <h2>Detalhes do Pedido</h2>
-      {boleto && <p>ID do Pedido do boleto: {boleto._id}</p>}
-      {pix && <p>ID do Pedido do pix: {pix._id}</p>}
-      {creditCard && <p>ID do Pedido do cartao de credito: {creditCard._id}</p>}
+      {boleto && (
+        <div>
+          <h2 style={{ marginLeft: "4rem" }}>Detalhes do Pedido</h2>
 
-      {/* Adicione outros detalhes do pedido conforme necessário */}
+          <table
+            style={{
+              margin: "0 auto",
+              width: "90dvw",
+              marginTop: "3rem",
+              borderCollapse: "collapse",
+              border: "1px solid #dddddd",
+              marginBottom: "3rem",
+            }}
+          >
+            <thead>
+              <tr style={{ padding: "1rem" }}>
+                <th style={{ padding: ".8rem", width: "15vw" }}>produtos</th>
+                <th style={{ width: "40vw" }}>Tamanho</th>
+                <th style={{ whiteSpace: "nowrap" }}>Tipo</th>
+                <th style={{ width: "15vw" }}>quantidade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {boleto.products.map((order, prodIndex) => (
+                <tr key={prodIndex}>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    <img
+                      src={order.image}
+                      alt={`Produto ${order.productId}`}
+                      style={{ width: "10vw" }}
+                    />
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {order.size}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {boleto.billingType}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {order.quantity}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <h1 style={{ marginLeft: "4rem" }}>frete do pedido</h1>
+
+          <table
+            style={{
+              margin: "0 auto",
+              width: "90dvw",
+              marginTop: "3rem",
+              borderCollapse: "collapse",
+              border: "1px solid #dddddd",
+              marginBottom: "3rem",
+            }}
+          >
+            <thead>
+              <tr style={{ padding: "1rem" }}>
+                <th style={{ padding: ".8rem", width: "15vw" }}>
+                  transportadora
+                </th>
+
+                <th style={{ width: "15vw" }}>logo</th>
+                <th style={{ width: "40vw" }}>Preço do frete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {boleto.shippingFeeData && (
+                <tr>
+                  <img
+                    src={boleto.shippingFeeData.logo}
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                      width: "20vw",
+                    }}
+                  ></img>
+
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {boleto.shippingFeeData.transportadora}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {boleto.shippingFeeData.shippingFeePrice}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {pix && (
+        <div>
+          <h2 style={{ marginLeft: "4rem" }}>Detalhes do Pedido</h2>
+
+          <table
+            style={{
+              margin: "0 auto",
+              width: "90dvw",
+              marginTop: "3rem",
+              borderCollapse: "collapse",
+              border: "1px solid #dddddd",
+              marginBottom: "3rem",
+            }}
+          >
+            <thead>
+              <tr style={{ padding: "1rem" }}>
+                <th style={{ padding: ".8rem", width: "15vw" }}>produtos</th>
+                <th style={{ width: "40vw" }}>Tamanho</th>
+                <th style={{ whiteSpace: "nowrap" }}>Tipo</th>
+                <th style={{ width: "15vw" }}>quantidade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pix.products.map((order, prodIndex) => (
+                <tr key={prodIndex}>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    <img
+                      src={order.image}
+                      alt={`Produto ${order.productId}`}
+                      style={{ width: "10vw" }}
+                    />
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {order.size}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {pix.billingType}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {order.quantity}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <h1 style={{ marginLeft: "4rem" }}>frete do pedido</h1>
+
+          <table
+            style={{
+              margin: "0 auto",
+              width: "90dvw",
+              marginTop: "3rem",
+              borderCollapse: "collapse",
+              border: "1px solid #dddddd",
+              marginBottom: "3rem",
+            }}
+          >
+            <thead>
+              <tr style={{ padding: "1rem" }}>
+                <th style={{ padding: ".8rem", width: "15vw" }}>
+                  transportadora
+                </th>
+
+                <th style={{ width: "15vw" }}>logo</th>
+                <th style={{ width: "40vw" }}>Preço do frete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pix.shippingFeeData && (
+                <tr>
+                  <img
+                    src={pix.shippingFeeData.logo}
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                      width: "20vw",
+                    }}
+                  ></img>
+
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {pix.shippingFeeData.transportadora}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {pix.shippingFeeData.shippingFeePrice}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      )}
+
+      {creditCard &&  <div>
+          <h2 style={{ marginLeft: "4rem" }}>Detalhes do Pedido</h2>
+
+          <table
+            style={{
+              margin: "0 auto",
+              width: "90dvw",
+              marginTop: "3rem",
+              borderCollapse: "collapse",
+              border: "1px solid #dddddd",
+              marginBottom: "3rem",
+            }}
+          >
+            <thead>
+              <tr style={{ padding: "1rem" }}>
+                <th style={{ padding: ".8rem", width: "15vw" }}>produtos</th>
+                <th style={{ width: "40vw" }}>Tamanho</th>
+                <th style={{ whiteSpace: "nowrap" }}>Tipo</th>
+                <th style={{ width: "15vw" }}>quantidade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {creditCard.products.map((order, prodIndex) => (
+                <tr key={prodIndex}>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    <img
+                      src={order.image}
+                      alt={`Produto ${order.productId}`}
+                      style={{ width: "10vw" }}
+                    />
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {order.size}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {creditCard.billingType}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {order.quantity}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <h1 style={{ marginLeft: "4rem" }}>frete do pedido</h1>
+
+          <table
+            style={{
+              margin: "0 auto",
+              width: "90dvw",
+              marginTop: "3rem",
+              borderCollapse: "collapse",
+              border: "1px solid #dddddd",
+              marginBottom: "3rem",
+            }}
+          >
+            <thead>
+              <tr style={{ padding: "1rem" }}>
+                <th style={{ padding: ".8rem", width: "15vw" }}>
+                  transportadora
+                </th>
+
+                <th style={{ width: "15vw" }}>logo</th>
+                <th style={{ width: "40vw" }}>Preço do frete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {creditCard.shippingFeeData && (
+                <tr>
+                  <img
+                    src={creditCard.shippingFeeData.logo}
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                      width: "20vw",
+                    }}
+                  ></img>
+
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {creditCard.shippingFeeData.transportadora}
+                  </td>
+                  <td
+                    style={{
+                      borderLeft: "1px solid #dddddd",
+                      borderBottom: "1px solid #dddddd",
+                      padding: "1rem",
+                    }}
+                  >
+                    {creditCard.shippingFeeData.shippingFeePrice}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>}
     </div>
   );
 };
